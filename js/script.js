ad9c7e918129fef22ef8b663444c19ae97498f76
oberleaidan18 <oberle.aidan@gmail.com>
2025-12-10 22:25:48 -0500

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,31 @@
+// Create waveform
+const wavesurfer = WaveSurfer.create({
+    container: '#waveform',
+    waveColor: '#fef3ed',
+    progressColor: '#c46b47',
+    height: 30,
+    barWidth: 2,
+    responsive: true
+});
+
+// Load your audio file
+wavesurfer.load('media/skyline.mp3');
+
+// Hook up play/pause button
+const playPauseBtn = document.getElementById('play-pause');
+const playPauseIcon = document.getElementById('play-pause-icon');
+
+playPauseBtn.addEventListener('click', () => {
+    wavesurfer.playPause();
+
+    if (wavesurfer.isPlaying()) {
+        playPauseIcon.src = "images/pause.png";
+    } else {
+        playPauseIcon.src = "images/play.png";
+    }
+});
+
+// Optional: change icon when song ends
+wavesurfer.on('finish', () => {
+    playPauseIcon.src = "images/play.png";
+});
